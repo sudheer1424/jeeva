7d27f6d62f7d261c63e350461694d82a403b5cef
sudeer1424 <sudheerkumar1424@gmail.com>
2013-12-20 13:11:16 +0530

    this is first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jeeva::Application.config.secret_key_base = 'dfd2535c65db9db4d47083d78c78e9a7793ba0410e82fa399c975050604869b387743a208140871b68cbb9ba2418f2ae314afed9171aed17afbad36e13aa6014'
